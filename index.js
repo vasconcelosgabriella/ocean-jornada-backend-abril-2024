50b58b60e47e7170ff257f5e5c65a430c58a98f4
Gabriella Vasconcelos <134015236+vasconcelosgabriella@users.noreply.github.com>
2024-04-16 14:42:17 +0000

    Criação do endpoint `/oi`

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello, world")
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+// Exercício: Criar um endpoint [GET] /oi que exibe: "Olá, mundo!"
+app.get('/oi', function (req, res) {
+    res.send('Olá, mundo!')
+})
+app.listen(3000)
